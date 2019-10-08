e8faf6a93bb2370169638d02f7e7c15dafd2a73e
Cameron Lattz <camerolattz@gmail.com>
2019-10-08 14:34:23 -0500

    adding handlebars

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/controller.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+
+module.exports = app;
