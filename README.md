99adc4b329771bfb57a3f973078606f411035ec9
Cameron Lattz <camerolattz@gmail.com>
2019-10-10 17:31:49 -0500

    final styling fixes; wrote readme; organized and commented js and css

--- README.md
+++ README.md
@@ -1,26 +1,49 @@
-# AllTheNewsThatsFitToScrape
+# TheDailyScraper
 https://daily-scraper-cl.herokuapp.com/
 
-### TODO:
-Restructure code
-Comment code
-Write readme
+The Daily Scraper is an app that scrapes RSS news feeds and displays their articles on one page.
 
-Eat-Da-Burger is an app that lets you eat burgers!
-Enter the name of a burger in the input on the bottom to add it to the list on the left.
-Click a burger on the left to eat it!
+### **USAGE:**
+**News feeds:**  
+To add a news feed, first add the site's name to the "Site name" input box on the top right. Then paste an RSS feed url into the "Feed url" input and hit enter or the "Add Feed" button.  
+To remove a news feed, click the "x" button next to the site name in the article list.  
+NOTE: Not all RSS feeds will work. Some, especially ones that are wrapped in a JS function, are not formatted in a way that the scraper can read.
 
-### OVERVIEW:
-This is app utilizes a MySQL database, Express/Handlebars routing, and an MVC design pattern. The database and server are hosted on Heroku.
+**Comments:**  
+To add a comment, enter a comment into the text box underneath an article and click "Add Comment".  
+To remove a comment, click the "x" button next to the comment you want to remove.
 
-A request to the root directory grabs a list of burgers from the MySQL database and serves up the *"index.html"* page with that burger data. This, and all other SQL calls are made through a custom ORM. The *"index.html"* layout is built using *"main.handlebars"* and "*index.handlebars*". Animations and further API calls to eat a burger or add a new burger are done using JavaScript.
+**Filtering:**  
+To filter news articles, type keywords (separated by a comma) in the "Keyword" input on the top right then click "Filter". If you don't use a comma, multiple words (separated by spaces) will be formatted as a single keyword.  
+To remove a filter, clear the "Keyword" input and click "Filter" again.
 
-### TECHNOLOGIES USED:
-* Node
-* Express Node module
-* Express-Handlebars Node module
-* MySQL
+### **OVERVIEW:**
+This is app utilizes a Mongo database, Express/Handlebars routing, and an MVC design pattern. The database and server are hosted on Heroku.
+
+A request to the root directory grabs a list of scraped articles from the Mongo database and serves up the *"index.html"* page with that article data. This, and all other Mongo calls are validated using Mongoose. The *"index.html"* layout is built using *"main.handlebars"* and "*index.handlebars*". Further API calls to add or remove a feed or comment, or to re-scrape articles, are done using JavaScript.
+
+RSS feeds are formatted using *https://rss.bloople.net/*, which converts the RSS into HTML. The resulting HTML is then scraped by Cheerio, and all articles are added to the database.
+
+### **PARTIAL LIST OF WORKING RSS FEEDS:**
+* BBC: http://feeds.bbci.co.uk/news/rss.xml
+* CBS Sports: https://rss.cbssports.com/rss/headlines/
+* CNBC: https://www.cnbc.com/id/15837362/device/rss/rss.html
+* CNN: http://rss.cnn.com/rss/cnn_allpolitics.rss
+* Fox News: http://feeds.foxnews.com/foxnews/politics
+* Reuters: http://feeds.reuters.com/Reuters/PoliticsNews
+* Time: http://feeds.feedburner.com/timeblogs/swampland
+* Wall Street Journal: https://feeds.a.dj.com/rss/RSSWorldNews.xml
+* Washington Post: http://feeds.washingtonpost.com/rss/politics?tid=lk_inline_manual_2
+* Wired: https://www.wired.com/feed/rss
+
+### **TECHNOLOGIES USED:**
+* Cheerio
+* Express
+* Express-Handlebars
 * Heroku
+* Node
+* Mongo
+* Mongoose
 
-### TEAM:
+### **TEAM:**
 * Cameron Lattz, Developer
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,8 @@
 (function() {
+    // "global" variable to store articles from the database
     let _articles = [];
 
+    // add a comment to the database and page
     const _addComment = function(event) {
         event.preventDefault();
         const form = event.target;
@@ -16,15 +18,17 @@
             return response.json();
         }).then(function() {
             const ul = form.getElementsByTagName("ul")[0];
-            _buildComments(ul, [body.comment])
+            _displayComments(ul, [body.comment])
             form.reset();
         })
         .catch(function() {});
     }
 
+    // add a feed to the database and scrape news
     const _addFeed = function(event) {
         event.preventDefault();
         const sites = [...new Set(_articles.map(article => article.site))].sort();
+        // only allow adding a feed if there are less than 5 feeds
         if (sites.length < 5) {
             const input = event.target;
             const form = input.closest("form");
@@ -49,50 +53,32 @@
         }
     }
 
+    // show an error notification
     const _alert = function(message) {
         const alertDiv = document.getElementById("alertDiv");
         alertDiv.innerText = message;
         alertDiv.classList.add("show");
         setTimeout(function() {
             alertDiv.classList.remove("show");
+            setTimeout(function() {
+                alertDiv.innerText = "";
+            }, 500);
         }, 3000);
     }
 
-    const _buildComments = function(ul, comments) {
-        const lis = comments.map(comment => {
-            const li = document.createElement("li");
-            li.innerText = comment;
-            const link = document.createElement("a");
-            link.innerText = "x";
-            link.classList.add("x");
-            link.addEventListener("click", _removeComment);
-            li.append(link);
-            return li;
-        });
-        const existingComments = ul.getElementsByTagName("li");
-        if (existingComments.length === 0 && comments.length > 0) {
-            const lh = document.createElement("lh");
-            lh.innerText = "Comments:";
-            lis.unshift(lh);
-        }
-        ul.append(...lis)
-        return ul;
-    }
-
-    const _displayArticles = function(articles, updateFeeds = true) {
-        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        const today = new Date();
-        const date = days[today.getDay()] + ", " + months[today.getMonth()+1] + " " + today.getDate() + ", " + today.getFullYear();
-        document.getElementById("subHeader").innerText = date;
+    // show articles on the page
+    const _displayArticles = function(articles, filtered) {
+        _displayDate();
         const articlesTr = document.getElementById("articles");
         articlesTr.innerHTML = "";
+        // get a list of unique site names
         const sites = [...new Set(articles.map(article => article.site))].sort();
+        // if there are no site names, we either haven't added any feeds or the filter returned no results
         if (sites.length === 0) {
             const td = document.createElement("td");
             const h3 = document.createElement("h3");
             h3.classList.add("no-feeds");
-            h3.innerText = "No feeds added. Please add an RSS feed to continue.";
+            h3.innerText = filtered ? "No results found." : "No feeds added. Please add an RSS feed to continue.";
             td.append(h3);
             articlesTr.append(td);
         }
@@ -108,19 +94,24 @@
             link.addEventListener("click", _removeFeed);
             h3.append(link);
             siteTd.append(h3);
+            // filter articles by currently iterated site
             const siteArticles = articles.filter(article => article.site === site);
             siteArticles.forEach(article => {
+                // clone the article template
                 const clone = document.getElementsByClassName("articleForm")[0].cloneNode(true);
                 const link = clone.getElementsByClassName("link")[0];
                 link.href = article.url;
                 link.innerText = article.title;
                 const lastSummaryCharacter = article.summary.charAt(article.summary.length-1);
+                // if the last character of the summary has no punctuation, add a period
                 if (![".", "!", "?", ",", ":", ";", "…"].includes(lastSummaryCharacter)) {
                     article.summary += ".";
                 }
                 clone.getElementsByClassName("summary")[0].innerText = article.summary;
                 const ul = clone.getElementsByClassName("comments")[0];
-                _buildComments(ul, article.comments);
+                // add LIs with comments in them to the comments list
+                _displayComments(ul, article.comments);
+                // put the article id on the form so we can use it later
                 clone.setAttribute("id", article._id);
                 clone.addEventListener("submit", _addComment);
                 siteTd.append(clone);
@@ -129,32 +120,71 @@
         });
     }
 
+    // show comments on the article
+    const _displayComments = function(ul, comments) {
+        // build LIs with the comments
+        const lis = comments.map(comment => {
+            const li = document.createElement("li");
+            li.innerText = comment;
+            const link = document.createElement("a");
+            link.innerText = "x";
+            link.classList.add("x");
+            link.addEventListener("click", _removeComment);
+            li.append(link);
+            return li;
+        });
+        // if no comments are already displayed on the article, and we are adding one now, add a header to the list
+        const existingComments = ul.getElementsByTagName("li");
+        if (existingComments.length === 0 && comments.length > 0) {
+            const lh = document.createElement("lh");
+            lh.innerText = "Comments:";
+            lis.unshift(lh);
+        }
+        ul.append(...lis)
+        return ul;
+    }
+
+    // show today's date on the page
+    const _displayDate = function() {
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const today = new Date();
+        const date = days[today.getDay()] + ", " + months[today.getMonth()+1] + " " + today.getDate() + ", " + today.getFullYear();
+        document.getElementById("date").innerText = date;
+    }
+
+    // filter articles by keywords
     const _filterArticles = function(event) {
         event.preventDefault();
         const form = event.target;
         const keywords = form.keywords.value.split(",");
         const filteredArticles = _articles.filter(article => {
+            // return true if at least one keyword appears in the summary or title
             return keywords.some(keyword => {
-                if (article.summary.toLowerCase().indexOf(keyword) !== -1) {
+                if (article.summary.toLowerCase().indexOf(keyword.toLowerCase()) !== -1) {
                     return true;
-                } else if (article.title.toLowerCase().indexOf(keyword) !== -1) {
+                } else if (article.title.toLowerCase().indexOf(keyword.toLowerCase()) !== -1) {
                     return true;
                 }
             });
         });
-        _displayArticles(filteredArticles, false);
+        _displayArticles(filteredArticles, true);
     }
 
+    // function that runs when the page finishes loading
     const _init = function() {
         const scrapeImg = document.getElementById("scrapeImg");
         scrapeImg.classList.add("spin");
+        _displayDate();
         scrapeImg.addEventListener("click", _scrape);
         document.getElementById("addFeedForm").addEventListener("submit", _addFeed);
         document.getElementById("filterForm").addEventListener("submit", _filterArticles);
+        // grab already-scraped articles from the database
         fetch("api/articles").then(function(response) {
             return response.json();
         }).then(function(articles){
             _articles = articles;
+            // if no articles exist on the database, scrape them, otherwise show the articles
             if (articles.length === 0) {
                 _scrape();
             } else {
@@ -164,6 +194,7 @@
         });
     }
 
+    // delete a comment from an article
     const _removeComment = function(event) {
         const a = event.target;
         const text = a.previousSibling.textContent;
@@ -182,19 +213,22 @@
             const ul = a.closest("ul");
             a.closest("li").remove();
             const lis = ul.getElementsByTagName("li");
+            // if there are no comments, don't show the "Comments:" header
             if (lis.length === 0) {
                 ul.getElementsByTagName("lh")[0].remove();
             }
         });
     }
 
+    // delete a feed from the database and remove all scraped articles with that site
     const _removeFeed = function(event) {
+        // get a list of unique site names
         const sites = [...new Set(_articles.map(article => article.site))].sort();
         if (sites.length > 3) {
             const a = event.target;
-            const text = a.previousSibling.textContent;
+            const site = a.previousSibling.textContent;
             const body = {
-                name: text
+                name: site
             }
             fetch("api/feeds/", {
                 method: "DELETE",
@@ -205,13 +239,16 @@
                 return response.json();
             }).then(function(responseJson) {
                 a.closest("h3").remove();
-                _scrape();
+                // remove articles taken from the deleted site and re-render them
+                _articles = _articles.filter(article => article.site !== site);
+                _displayArticles(_articles);
             });
         } else {
             _alert("You cannot have less than 3 RSS feeds.");
         }
     }
 
+    // scrape the RSS feeds and display the articles
     const _scrape = function() {
         document.getElementById("articles").innerHTML = "";
         document.getElementById("filterForm").keywords.value = "";
@@ -230,5 +267,6 @@
         });
     }
 
+    // run _init when page loads
     document.addEventListener("DOMContentLoaded", _init);
 })();
